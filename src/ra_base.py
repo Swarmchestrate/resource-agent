742d3894e0d364f033714d518ed726c3ae38d26f
ZeWang <z.wang6@lboro.ac.uk>
2026-02-04 14:48:03 +0000

    test

--- src/ra_base.py
+++ src/ra_base.py
@@ -635,6 +635,7 @@ class ResourceAgent:
         ]
         self.lead_resource[job_id] = random.choice(valid) if valid else None
         #self.lead_resource[job_id] = next((k for k, v in self.job_offers[job_id].items() if v.get('ra_id') == 'ra-sztaki-cloud-hu'), None)
+        self.lead_resource[job_id] = next((k for k, v in self.job_offers[job_id].items() if v.get('ra_id') == 'ra-aws-cloud-us'), None)
         LR_id = self.job_offers[job_id][self.lead_resource[job_id]]["ra_id"]
         provider = self.job_offers[job_id][self.lead_resource[job_id]]["provider"]
         instance_type = self.job_offers[job_id][self.lead_resource[job_id]]["instance_type"]
