2837786084b59cdcfca8fdbe1f8e23b39f5cc017
ZeWang <z.wang6@lboro.ac.uk>
2026-04-10 14:52:44 +0100

    debug

--- src/ra_base.py
+++ src/ra_base.py
@@ -1472,8 +1472,9 @@ class ResourceAgent:
         print(f"[DEBUG] instance_type is {instance_type}, k3s_role is {k3s_role}, resource_name is {resource_name}, cluster_name is {cluster_name}, master_ip is {master_ip}, k3s_token is {k3s_token}")
         
         
-    # Get the offer info of the resource(s) to deploy
-        resource_offer = {resource_name: offer_data}
+        # TODO: for worker nodes, do the same as master node
+        # Get the offer info of the resource(s) to deploy
+        resource_offer = {resource_name: {offer_data["ids"]["offer_id"]: offer_data}}
 
         print(f"offer_info received by worker node is {resource_offer}")
 
@@ -1510,8 +1511,6 @@ class ResourceAgent:
         openstack_network_id = node_info.get("openstack_network_id", "")
         openstack_flavor_name = node_info.get("flavor_name", "")
 
-        print(f"ssh_user is {ssh_user}, ssh_key_path is {ssh_key_path}")
-
         
         
         for i in range(1):
@@ -1527,12 +1526,12 @@ class ResourceAgent:
                     f'{{"cloud": "{cloud}",' # Ze: we can make it dynamic fetch from offer. Each RA could access multiple providers so this cannot be collected from config file
                     f'"instance_type": "{instance_type}",'
                     f'"ha": false,'
-                    f'"ami": "{aws_ami}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own ami?
+                    f'"ami": "{self.aws_ami}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own ami?
                     f'"security_group_id": "",' # Ze: we can make it dynamic later from cluster-builder lib
                     f'"resource_name":"{node_name}",' # Ze: to think about how to name
-                    f'"ssh_user": "{ssh_user}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own ssh user?
+                    f'"ssh_user": "{self.ssh_user}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own ssh user?
                  #   f'"ssh_key_name": "",' # Ze: we can make it dynamic later (from capacity/config info) Does each provider has its own key pair?
-                    f'"ssh_key": "{ssh_key_path}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own private key?
+                    f'"ssh_key": "{self.ssh_key_path}",' # Ze: we can make it dynamic later (from capacity/config info) does each provider has its own private key?
                     f'"k3s_role": "{k3s_role}",' # Ze: this should be default 
                     f'"k3s_token": "{k3s_token}",'
                     f'"master_ip": "{master_ip}",'
@@ -1543,17 +1542,17 @@ class ResourceAgent:
                     f'{{"cloud": "{cloud}",'
                     f'"openstack_flavor_id": "{instance_type}",'
                     f'"ha": false,'
-                    f'"openstack_image_id": "{openstack_image_id}",'
+                    f'"openstack_image_id": "{self.openstack_image_id}",'
                     #f'"security_group_id": "",'
                     f'"volume_size": "10",'
                     #f'"floating_ip_pool": "ext-net",'
-                    f'"network_id": "{openstack_network_id}",'
+                    f'"network_id": "{self.openstack_network_id}",'
                     f'"resource_name":"{node_name}",'    
-                    f'"ssh_user": "{ssh_user}",'
+                    f'"ssh_user": "{self.ssh_user}",'
                     #f'"ssh_key_name": "",'
-                    f'"ssh_key": "{ssh_key_path}",'
+                    f'"ssh_key": "{self.ssh_key_path}",'
                     f'"use_block_device": true,'
-                    f'"k3s_role": "{k3s_role}",'
+                    f'"k3s_role": "worker",'
                     f'"k3s_token": "{k3s_token}",'
                     f'"master_ip": "{master_ip}",'
                     f'"cluster_name": "{cluster_name}"}}' 
@@ -1561,13 +1560,13 @@ class ResourceAgent:
 
             worker_node_edge = (
                     f'{{"cloud": "{cloud}",'
-                    f'"edge_device_ip": "{edge_device_ip}",'
+                    f'"edge_device_ip": "{self.edge_device_ip}",'
                     f'"ha": false,'
                     f'"resource_name":"{node_name}",'
-                    f'"ssh_user": "{ssh_user}",'
-                    f'"ssh_key": "{ssh_key_path}",'
-                    f'"ssh_auth_method": "{ssh_auth_method or "key"}",'
-                    f'"k3s_role": "{k3s_role}",'
+                    f'"ssh_user": "{self.ssh_user}",'
+                    f'"ssh_key": "{self.ssh_key_path}",'
+                    f'"ssh_auth_method": "key",'
+                    f'"k3s_role": "worker",'
                     f'"k3s_token": "{k3s_token}",'
                     f'"master_ip": "{master_ip}",'
                     f'"cluster_name": "{cluster_name}"}}'   
@@ -1575,10 +1574,9 @@ class ResourceAgent:
             worker_node = {
                     "aws": worker_node_aws,
                     "openstack": worker_node_openstack,
-                    "sztaki": worker_node_openstack,
                     "edge": worker_node_edge
                 }[cloud]
-            print(f"ssh_user is {ssh_user}")
+            print(f"ssh_user is {self.ssh_user}")
             
             
             worker_node = json.loads(worker_node)
